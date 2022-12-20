966443e4f2620906743fffa0a0c31ac1447c27f3
Ali Yalama <aliyalama@googlemail.com>
2022-12-20 03:26:35 +0100

    Write SeasonService

---
+++ src/main/java/com/example/mongodbtutorial/Season/service/SeasonServiceImpl.java
@@ -0,0 +1,51 @@
+package com.example.mongodbtutorial.Season.service;
+
+import com.example.mongodbtutorial.Season.model.Season;
+import com.example.mongodbtutorial.Season.repository.SeasonRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class SeasonServiceImpl implements SeasonService{
+
+    @Autowired
+    private SeasonRepository seasonRepository;
+
+    @Override
+    public Season findById(String id) {
+        return seasonRepository.findById(id).get();
+    }
+
+    @Override
+    public List<Season> findAll() {
+        return seasonRepository.findAll();
+    }
+
+    @Override
+    public Season findByYear(Long year) {
+        return seasonRepository.findSeasonByYear(year).get();
+    }
+
+    @Override
+    public Season save(Season season) {
+        return seasonRepository.save(season);
+    }
+
+    @Override
+    public Season update(Season season) {
+        //leaving out special update logic because this is just a demo
+        return seasonRepository.save(season);
+    }
+
+    @Override
+    public String delete(Season season) {
+        seasonRepository.delete(season);
+        return season.getId();
+    }
+}
--- src/main/java/com/example/mongodbtutorial/shared/models/BaseDocument.java
+++ src/main/java/com/example/mongodbtutorial/shared/models/BaseDocument.java
@@ -1,7 +1,10 @@
 package com.example.mongodbtutorial.shared.models;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
-
+@Getter
+@Setter
 public abstract class BaseDocument {
     @Id
     private String id;
