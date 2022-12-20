217420c5ab4ceff89ddba4d034b3e1af289ddfa9
Ali Yalama <aliyalama@googlemail.com>
2022-12-20 04:46:54 +0100

    comment Out Repository executions so no doubling data

--- src/main/java/com/example/mongodbtutorial/MongoDbTutorialApplication.java
+++ src/main/java/com/example/mongodbtutorial/MongoDbTutorialApplication.java
@@ -40,10 +40,12 @@ public class MongoDbTutorialApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Driver lewis = driverRepository.save(new Driver("Lewis", "Hamilton"));
-        Driver max = driverRepository.save(new Driver("Max", "Verstappen"));
-        Driver fernando = driverRepository.save(new Driver("Fernando", "Alonso"));
-        Driver charles = driverRepository.save(new Driver("Charles", "LeClerc"));
+        Driver lewis = new Driver("Lewis", "Hamilton");
+        Driver max = new Driver("Max", "Verstappen");
+        Driver fernando = new Driver("Fernando", "Alonso");
+        Driver charles = new Driver("Charles", "LeClerc");
+
+        //driverRepository.saveAll(Arrays.asList(lewis, max, fernando, charles));
 
         Car merc0 = new Car(1025, "Mercedes-AMG F1 W11 EQ Performance");
         Car merc1 = new Car(1050, "Mercedes W12");
@@ -59,14 +61,14 @@ public class MongoDbTutorialApplication implements CommandLineRunner {
         Car renault0 = new Car(950, "Alpine A521");
         Car renault1 = new Car(950, "Alpine A522");
 
-        carRepository.saveAll(Arrays.asList(merc0, merc1, merc2, rb0, rb1, ferrari0, ferrari1, ferrari2, renault0, renault1));
+        //carRepository.saveAll(Arrays.asList(merc0, merc1, merc2, rb0, rb1, ferrari0, ferrari1, ferrari2, renault0, renault1));
 
         Team alpine = new Team("BWT Alpine F1 Team", Arrays.asList(renault0, renault1));
         Team ferrari = new Team("Scuderia Ferrari", Arrays.asList(ferrari0, ferrari1, ferrari2));
         Team redbull = new Team("Red Bull Racing", Arrays.asList(rb0, rb1));
         Team mercedes = new Team("Mercedes-AMG Petronas F1 Team", Arrays.asList(merc0, merc1, merc2));
 
-        teamRepository.saveAll(Arrays.asList(alpine, ferrari, redbull, mercedes));
+        //teamRepository.saveAll(Arrays.asList(alpine, ferrari, redbull, mercedes));
 
         Result ferrari2020 = new Result(ferrari, charles, 98, 8);
         Result redbull2020 = new Result(redbull, max, 214, 3);
@@ -82,15 +84,15 @@ public class MongoDbTutorialApplication implements CommandLineRunner {
         Result redbull2022 = new Result(redbull, max, 454, 1);
         Result mercedes2022 = new Result(mercedes, lewis, 240, 6);
 
-        resultRepository.saveAll(Arrays.asList(
+        /*resultRepository.saveAll(Arrays.asList(
                 ferrari2020, redbull2020, mercedes2020,
                 alpine2021, ferrari2021, redbull2021, mercedes2021,
-                alpine2022, ferrari2022, redbull2022, mercedes2022));
+                alpine2022, ferrari2022, redbull2022, mercedes2022));*/
 
         Season season2020 = new Season(2020L, Arrays.asList(ferrari2020, redbull2020, mercedes2020));
         Season season2021 = new Season(2021L, Arrays.asList(alpine2021, ferrari2021, redbull2021, mercedes2021));
         Season season2022 = new Season(2022L, Arrays.asList(alpine2022, ferrari2022, redbull2022, mercedes2022));
 
-        seasonRepository.saveAll(Arrays.asList(season2020, season2021, season2022));
+        //seasonRepository.saveAll(Arrays.asList(season2020, season2021, season2022));
     }
 }
